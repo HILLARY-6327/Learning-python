230a664f418d3d64b73815083238bb32376b1d28
HILLARY <hillaryimbukwa@gmail.com>
2025-04-28 17:16:02 +0300

    check_even_odd.py

---
+++ check_even_odd.py
@@ -0,0 +1,10 @@
+#  a program that prints if a number is divisible by two
+
+number = input(" enter a number ")
+
+number = int(number)
+if number % 2 == 0:
+    print("even")
+else:
+    print("odd")
+    
