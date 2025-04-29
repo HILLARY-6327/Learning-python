95fd9f1e45b7ee0433d030c3ed800180b5b5432a
HILLARY <hillaryimbukwa@gmail.com>
2025-04-29 14:11:16 +0300

    checkodm_or_uda.py

---
+++ checkodm_or_uda.py
@@ -0,0 +1,12 @@
+# check if a number is odm or uda
+
+number = input(" enter a number ")
+number = int(number)
+
+if number > 5:
+    print("odm")
+elif number == 5:
+    print("wiper")
+else:
+    print("uda")
+
