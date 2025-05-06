a906d9fba3c50a90b97e0d025b2cc8a59ebac13c
HILLARY <hillaryimbukwa@gmail.com>
2025-05-06 06:36:09 +0300

    add_update_dict.py

---
+++ add_update_dict.py
@@ -0,0 +1,9 @@
+# A programme to add and a update a dictionary
+
+student_info = {'name': 'Hillary', 'age': 30}
+
+student_info ['course'] = 'otc'
+student_info ['age'] = 32
+
+print(student_info)
+
