6baf29de9d045f0c0a9f768173decd1ca5ae5252
HILLARY <hillaryimbukwa@gmail.com>
2025-05-06 06:14:44 +0300

    remove_key_dict.py

---
+++ remove_key_dict.py
@@ -0,0 +1,7 @@
+# a programme to remove a key from a dictionary
+
+employee = {'name': 'Hillary', 'position': 'Meteorologist', 'email': 'hillaryimbukwa@gmail.com'}
+
+del employee ['email']
+
+print(employee)
