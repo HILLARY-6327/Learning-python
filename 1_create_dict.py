a3fd97562936778922400675503baa53a3debdb5
HILLARY <hillaryimbukwa@gmail.com>
2025-05-06 04:23:33 +0300

    1_create_dict.py

---
+++ 1_create_dict.py
@@ -0,0 +1,11 @@
+# aA programe that takes an input from the user and creates a dictionary
+
+name = input('enter a name:')
+
+age = input('enter an age:')
+
+city = input('enter a city:')
+
+person_info = {'name': name, 'age': age, 'city': city}
+
+print(person_info)
